9160d73a9280332fc2ce781720d6e8b261de326e
Nehuen D'Angelo <nd-angel@f4r11s1.clusters.42paris.fr>
2026-02-10 14:25:43 +0100

    3

--- philosopher.h
+++ philosopher.h
@@ -6,7 +6,7 @@
 /*   By: nd-angel <nd-angel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/09 15:21:10 by nd-angel          #+#    #+#             */
-/*   Updated: 2026/02/09 22:10:11 by nd-angel         ###   ########.fr       */
+/*   Updated: 2026/02/10 14:24:40 by nd-angel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,7 @@ typedef struct args
 	unsigned int	time_to_eat;
 	unsigned int	time_to_sleep;
 	int				nb_meals;
+	int				dead;
 }	t_args;
 
 typedef struct philo
@@ -35,6 +36,7 @@ typedef struct philo
 	pthread_mutex_t	*left_fork;
 	pthread_mutex_t	*right_fork;
 	t_args			*args;
+	pthread_mutex_t	mutex;
 }	t_philo;
 
 int	check_arg(int argc, char **argv, t_args *args);
